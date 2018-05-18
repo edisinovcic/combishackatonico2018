3c448380fc5f5ddaabc7a0d74bf4710011265530
Alen Hrga <ahrga93@gmail.com>
2018-05-18 11:28:18 +0200

    Modify migrations

--- crowdsale/migrations/3_deploy_crowdsale.js
+++ crowdsale/migrations/3_deploy_crowdsale.js
@@ -1,31 +1,6 @@
 const CombisToken = artifacts.require("./CombisToken.sol");
 const CombisCrowdsale = artifacts.require("./CombisCrowdsale.sol");
 
-function latestTime() {
-    return web3.eth.getBlock('latest').timestamp;
-}
-
-const duration = {
-    seconds: function (val) {
-        return val
-    },
-    minutes: function (val) {
-        return val * this.seconds(60)
-    },
-    hours: function (val) {
-        return val * this.minutes(60)
-    },
-    days: function (val) {
-        return val * this.hours(24)
-    },
-    weeks: function (val) {
-        return val * this.days(7)
-    },
-    years: function (val) {
-        return val * this.days(365)
-    }
-};
-
 module.exports = function (deployer) {
     const wallet = web3.eth.accounts[0];
 
